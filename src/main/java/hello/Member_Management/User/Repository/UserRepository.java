6cb839a8f2f07292da950b723c0a9977039de758
yangwoohyeon <dngus0717@gmail.com>
2025-02-20 21:02:44 +0900

    username --> userId로 필드명 변경

--- src/main/java/hello/Member_Management/User/Repository/UserRepository.java
+++ src/main/java/hello/Member_Management/User/Repository/UserRepository.java
@@ -4,5 +4,5 @@ import hello.Member_Management.User.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> { //Spring Data JPA 사용
-    User findByUsername(String name); //이름으로 유저 찾기
+    User findByUserId(String userId); //이름으로 유저 찾기
 }
