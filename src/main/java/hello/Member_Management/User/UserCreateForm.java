f8c228077f5a402e16083e73ebf3a90bf933a2b3
yangwoohyeon <dngus0717@gmail.com>
2025-02-20 21:03:06 +0900

    userId 필드 생성

--- src/main/java/hello/Member_Management/User/UserCreateForm.java
+++ src/main/java/hello/Member_Management/User/UserCreateForm.java
@@ -14,6 +14,9 @@ public class UserCreateForm {
     @Size(min=3, max = 25)
     private String username;
 
+    @NotEmpty(message = "아이디는 필수항목입니다.")
+    private String id;
+
     @NotEmpty(message = "비밀번호는 필수항목입니다.")
     private String password1;
 
